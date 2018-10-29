c78c55686421b5c9e6eeadf9ae1e532cb594d8f0
André Berberich <Andre.Berberich@tecalliance.net>
2018-10-29 11:38:52 +0100

    Added test for user

---
+++ ticketsystem/webserver/data/user/user_test.go
@@ -0,0 +1,14 @@
+package user
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+/*
+	UserName should be returned.
+ */
+func TestUser_GetUserNameString(t *testing.T) {
+	user := User{UserId: 1, Mail: "test@test.de", FirstName: "Max", LastName: "Mustermann"}
+	assert.Equal(t, "Max Mustermann", user.GetUserNameString())
+}
