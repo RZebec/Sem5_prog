8233658ad38f70ebc1708449f79237456c3a7c98
Robert Zebec <zebec.robert@gmail.com>
2018-11-22 14:49:20 +0100

    Added email regex source

--- ticketsystem/webserver/webui/files/script/scriptHandler.go
+++ ticketsystem/webserver/webui/files/script/scriptHandler.go
@@ -34,6 +34,7 @@ var loginScript = `
 	}
 	
 	function validateEmail(email) {
+		//Source: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
   		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   		return re.test(email);
 	}
