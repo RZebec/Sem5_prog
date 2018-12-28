546614c9fef34e835704efea67c040c9a2514dff
Adrian Dumke <AdrianDumke@web.de>
2018-12-28 14:07:03 +0100

    send v.1

--- ticketsystem/webserver/data/mail/mail.go
+++ ticketsystem/webserver/data/mail/mail.go
@@ -19,11 +19,19 @@ type Mail struct {
 	SentTime time.Time
 }
 
+var senders = []string{"test1@gmx.de", "Oberheld.asdf@web.de", "horstChristianAnderson@zip.org", "css@asd.com", "ad.du@ff.de",
+	"kaka@lang.de", "abc.defg@hij.de", "blabla@bla.de", "labor@saft.de", "orange@blau.de", "hohlfruch@haze.de"}
+
+var recievers = []string{"test2@gmx.de", "SuperOberheld.asdf@web.de", "FranzhorstChristianAnderson@zip.org", "Terrorist@asd.com",
+	"ad.du@ff.de", "kakhaufen@lang.de", "abcbnm.defg@hij.de", "blabla@bla.de", "laborGrube@saft.de", "orange@blau.de", "hohlfruchtigerSaft@haze.de"}
+
 func (mail Mail) RandomMail(n int) []Mail {
 	mails := make([]Mail, n)
 	for i := 0; i < n; i++ {
 		mail.Subject = randomText(10)
 		mail.Content = randomText(50)
+		mail.Sender = senders[i]
+		mail.Receiver = recievers[i]
 		mails[i] = mail
 	}
 	return mails
@@ -34,7 +42,9 @@ func (mail Mail) ExplicitMail() []Mail {
 	mail.Subject = readEntry()
 	fmt.Print("Entry text: ")
 	mail.Content = readEntry()
-	fmt.Print("Enter your name")
+	fmt.Print("Enter your Reciever")
+	mail.Receiver = readEntry()
+	fmt.Print("Enter your SenderMail")
 	mail.Sender = readEntry()
 
 	mails := make([]Mail, 1)
