d589d2303e598deac3d87f51a8849a9243aa703e
BerbsInf2016A <berbs1989@gmail.com>
2018-12-28 13:34:11 +0100

    Started implementation of api authentication handler

---
+++ ticketsystem/webserver/api/apiKeyAuthenticationHandler.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"de/vorlesung/projekt/IIIDDD/shared"
+	"de/vorlesung/projekt/IIIDDD/ticketsystem/webserver/core"
+	"net/http"
+)
+
+type ApiKeyAuthenticationHandler struct {
+	Next           core.HttpHandler
+	ApiKeyResolver func() string
+}
+
+func (h *ApiKeyAuthenticationHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	for _, cookie := range req.Cookies() {
+		if cookie.Name == shared.AuthenticationCookieName {
+			apiKey := cookie.Value
+			if h.ApiKeyResolver() == apiKey {
+				h.Next.ServeHTTP(w, req)
+			} else {
+				w.WriteHeader(401)
+			}
+			break
+		}
+
+	}
+	w.WriteHeader(401)
+}
