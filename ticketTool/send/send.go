f9639af61c05473678a664b70e7683ae804d5503
Adrian Dumke <AdrianDumke@web.de>
2018-12-28 13:33:26 +0100

    nächster Schritt RandomMails
    HttpSchnittstelle jetzt genutzt

--- ticketTool/send/send.go
+++ ticketTool/send/send.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bufio"
+	"de/vorlesung/projekt/IIIDDD/ticketTool/client"
 	"de/vorlesung/projekt/IIIDDD/ticketTool/configuration"
 	"de/vorlesung/projekt/IIIDDD/ticketsystem/logging"
 	"de/vorlesung/projekt/IIIDDD/ticketsystem/webserver/data/mail"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -23,46 +25,38 @@ func main() {
 		reader.ReadByte()
 		return
 	}
-
-
-	// TODO: REMOVE only for Test
-	/*apiClient, err := client.CreateClient(config)
-	if err != nil {
-		log.Fatal(err)
-	}
-	mails := *new([]mail.Mail)
-	mails = append(mails, mail.Mail{Sender: "test@test.de", Subject: "testsubject", Content: "testcontent"} )
-	err = apiClient.SendMails(mails)
-	if err != nil {
-		log.Fatal(err)
-	}*/
-	// TODO: Remove till here
-
-	fmt.Print("write Email or load Email ? (w/l):")
-	reader := bufio.NewReader(os.Stdin)
-	message := make([]byte, 100)
-
 	fmt.Print("write explicit mail or random mails ? (e/r):")
+
 	email := mail.Mail{}
 	eMails := []mail.Mail{}
-
 
 	for true {
 		answer := readEntry()
 		if answer == "e" {
 			eMails = email.ExplicitMail()
+			httpRequest(config, eMails)
+			break
 		} else if answer == "r" {
-			validateNumberOfRandomMails(email, eMails)
+			number := entryNumberOfRandomMails()
+			eMails = email.RandomMail(number)
+			httpRequest(config, eMails)
+			break
 		} else {
 			fmt.Print("wrong entry. Please press e or r: ")
 		}
 	}
 
-	fmt.Println("Email: ")
-	fmt.Println("")
-	fmt.Println("Start HTTPS-Request")
-
 }
+
+func httpRequest(config configuration.Configuration, eMails []mail.Mail) {
+	fmt.Println("Start HTTPS-Request")
+	apiClient, err := client.CreateClient(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	apiClient.SendMails(eMails)
+}
+
 func readEntry() string {
 	reader := bufio.NewReader(os.Stdin)
 	value, _ := reader.ReadString('\n')
@@ -70,16 +64,14 @@ func readEntry() string {
 	return value
 }
 
-func validateNumberOfRandomMails(email mail.Mail, eMails []mail.Mail) {
+func entryNumberOfRandomMails() int {
 	for true {
 		fmt.Println("Entry number of Random Mails: ")
 		number, err := strconv.Atoi(readEntry())
-
 		if err != nil {
 			fmt.Println("Entry is no Number!")
 		} else {
-			eMails = email.RandomMail(number)
-			break
+			return number
 		}
 	}
 }
