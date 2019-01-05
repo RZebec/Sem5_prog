c7e8eea95fd4b0e00a6e2a5b250e2e4a74decb6f
Robert Zebec <zebec.robert@gmail.com>
2019-01-05 13:08:35 +0100

    Api key setting feedback

--- ticketsystem/webserver/webui/admin/adminPageHandler_test.go
+++ ticketsystem/webserver/webui/admin/adminPageHandler_test.go
@@ -5,6 +5,7 @@ import (
 	"de/vorlesung/projekt/IIIDDD/ticketsystem/webserver/data/userData"
 	"de/vorlesung/projekt/IIIDDD/ticketsystem/webserver/testhelpers"
 	"de/vorlesung/projekt/IIIDDD/ticketsystem/webserver/webui/templateManager"
+	"de/vorlesung/projekt/IIIDDD/ticketsystem/webserver/webui/wrappers"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"net/http"
@@ -124,4 +125,56 @@ func TestAdminPageHandler_ServeHTTP_WrongRequest(t *testing.T) {
 
 	handler.ServeHTTP(rr, req)
 	assert.Equal(t, http.StatusMethodNotAllowed, rr.Code, "Status code 405 should be returned")
+}
+
+/*
+	Failed change should be shown.
+*/
+func TestAdminPageHandler_ServeHTTP_FailedChange(t *testing.T) {
+	req, err := http.NewRequest("GET", "/admin?IsChangeFailed=yes", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testLogger := testhelpers.GetTestLogger()
+
+	lockedUsers := []userData.User{{"Test@Test.de", 1,
+		"Test", "Test", userData.RegisteredUser, userData.WaitingToBeUnlocked}}
+
+	data := adminPageData{
+		Users:              lockedUsers,
+		IncomingMailApiKey: "1234",
+		OutgoingMailApiKey: "4321",
+		IsChangeFailed:		"yes",
+	}
+	data.UserIsAdmin = true
+	data.UserIsAuthenticated = true
+	data.Active = "admin"
+
+	mockedApiContext := new(mockedForTests.MockedApiConfiguration)
+
+	mockedApiContext.On("GetIncomingMailApiKey").Return("1234")
+
+	mockedApiContext.On("GetOutgoingMailApiKey").Return("4321")
+
+	mockedTemplateManager := new(templateManager.MockedTemplateManager)
+
+	mockedTemplateManager.On("RenderTemplate", mock.Anything, mock.Anything, data).Return(nil)
+
+	mockedUserContext := new(mockedForTests.MockedUserContext)
+
+	mockedUserContext.On("GetAllLockedUsers").Return(lockedUsers)
+
+	rr := httptest.NewRecorder()
+
+	testee := AdminPageHandler{UserContext: mockedUserContext, Logger: testLogger, TemplateManager: mockedTemplateManager, ApiContext: mockedApiContext}
+
+	handler := http.HandlerFunc(testee.ServeHTTP)
+	ctx := wrappers.NewContextWithAuthenticationInfo(req.Context(), true, true, 1,"")
+	handler.ServeHTTP(rr, req.WithContext(ctx))
+	assert.Equal(t, 200, rr.Code, "Status code 200 should be returned")
+
+	mockedApiContext.AssertExpectations(t)
+	mockedTemplateManager.AssertExpectations(t)
+	mockedUserContext.AssertExpectations(t)
 }
